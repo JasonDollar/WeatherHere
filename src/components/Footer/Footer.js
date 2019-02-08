07de6e0f7310cd51838398afc398a8821631a262
JasonDollar <elrey33@gmail.com>
2019-02-08 21:09:51 +0100

    small fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import classes from './Footer.module.scss'
 
 const Footer = () => (
   <div className={classes.Footer}>
-    <a href="https://darksky.net/poweredby/" className={classes.link}>Powered by DarkSky API &rarr;</a>
+    <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener noreferrer" className={classes.link}>Powered by DarkSky &rarr;</a>
   </div>
 )
 
